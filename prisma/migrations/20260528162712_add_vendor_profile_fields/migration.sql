9ce565d4822ebdfe1a975037f4fb277a82ea0e98
Joshua Holt <joshuaholt@Joshuas-MacBook-Air.local>
2026-05-28 11:27:14 -0500

    Add vendor profile fields

---
+++ prisma/migrations/20260528162712_add_vendor_profile_fields/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Vendor" ADD COLUMN     "bannerImage" TEXT,
+ADD COLUMN     "bio" TEXT,
+ADD COLUMN     "category" TEXT,
+ADD COLUMN     "instagram" TEXT,
+ADD COLUMN     "logo" TEXT,
+ADD COLUMN     "website" TEXT;
